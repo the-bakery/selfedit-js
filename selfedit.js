0d7b79831201b633c2ec1f5f029e24a54d409bf5
Mate J Kovacs <mkovaxx@gmail.com>
2013-02-08 21:57:04 -0800

    expand-collapse, minor optimizations

--- selfedit.js
+++ selfedit.js
@@ -1,63 +1,54 @@
 /* Copyright 2013, Mate J Kovacs */
 
-unfold('window', document.getElementById('view'), this);
+expand('window', document.getElementById('view'), this);
 
-function unfold(id, view, obj)
+function expand(id, view, obj)
 {
-    summary(id, view, obj, "-", function() { fold(id, view, obj) });
+    summary(id, view, obj, "collapser", function() { collapse(id, view, obj) });
     details(id, view, obj);
 }
 
-function fold(id, view, obj)
+function collapse(id, view, obj)
 {
-    summary(id, view, obj, "+", function() { unfold(id, view, obj) });
+    summary(id, view, obj, "expander", function() { expand(id, view, obj) });
 }
 
-function summary(id, view, obj, text, func)
+function summary(id, view, obj, style, func)
 {
     while (view.hasChildNodes()) { view.removeChild(view.lastChild); }
 
-    var button = document.createElement("button");
-    button.appendChild( document.createTextNode(text) );
-    button.setAttribute("style", "width:24px; text-align:left");
-    button.addEventListener("click", func);
-    view.appendChild(button);
+    var span = document.createElement("span");
+    span.setAttribute("class", style);
+    span.addEventListener("click", func);
     var type = (obj === null) ? "null" : Object.prototype.toString.call(obj);
-    view.appendChild( document.createTextNode(id + " : " + type) );
+    span.appendChild( document.createTextNode(id + " : " + type) );
+    view.appendChild(span);
 }
 
 function details(id, view, obj)
 {
-    var node = document.createElement("div");
+    var node = document.createElement("ul");
 
-    node.setAttribute("style", "margin-left:24px");
-    if (obj === null)
-    {
-        node.appendChild( document.createTextNode("null") );
-    }
-    else
-    {
-        var string = obj.toString();
-        node.appendChild( document.createTextNode(string) );
-        node.appendChild( document.createElement("br") );
+    var item = document.createElement("li");
+    var text = (obj === null) ? "null" : obj.toString();
+    item.appendChild( document.createTextNode(text) );
+    node.appendChild(item);
 
-        try
+    try
+    {
+        var keys = Object.getOwnPropertyNames(obj).sort();
+        node.appendChild(
+            document.createTextNode("properties (" + keys.length + "):")
+        );
+        for (i = 0; i < keys.length; i++)
         {
-            var keys = Object.getOwnPropertyNames(obj).sort();
-            node.appendChild(
-                document.createTextNode("properties (" + keys.length + "):")
-            );
-            node.appendChild( document.createElement("br") );
-            for (i = 0; i < keys.length; i++)
-            {
-                var key = keys[i];
-                var child = document.createElement("div");
-                fold(key, child, obj[key]);
-                node.appendChild(child);
-            }
+            var key = keys[i];
+            var child = document.createElement("li");
+            collapse(key, child, obj[key]);
+            node.appendChild(child);
         }
-        catch(e){}
     }
+    catch(e){}
 
     view.appendChild(node);
 }
